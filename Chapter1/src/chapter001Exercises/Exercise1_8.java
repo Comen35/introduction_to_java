8d3d59b39ab9913d4204217cd796c71a140a4a79
Comen35 <bahadir33@msn.com>
2022-05-31 18:48:14 +0300

    Chapter1 exercise 8 added

---
+++ Chapter1/src/chapter001Exercises/Exercise1_8.java
@@ -0,0 +1,11 @@
+package chapter001Exercises;
+// Write a program that displays the area o and perimter of a circle that has a radius of 5.5 using the folloving formulas:
+// perimeter = 2 x radius x radius
+// area = pi X radiıs x radius 
+
+public class Exercise1_8 {
+    public static void main(String[] args) {
+        System.out.println("Perimeter = " + (2 * 5.5 * 5.5 * 3.14));
+        System.out.println("Area      = " + (5.5 * 5.5 * 3.14));        
+    }
+}
