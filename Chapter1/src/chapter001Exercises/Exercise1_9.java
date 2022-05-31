b6b8c8d5e3c934d3d020559791545a1e2cbace0e
Comen35 <bahadir33@msn.com>
2022-05-31 18:55:23 +0300

    Chapter1 exercise 9 added

---
+++ Chapter1/src/chapter001Exercises/Exercise1_9.java
@@ -0,0 +1,11 @@
+package chapter001Exercises;
+
+/* Write a program that displays the area and perimeter of a rectangle with width 4.5 and a height 7.9 using followinf formula:
+    area = width x height
+*/
+public class Exercise1_9 {
+    public static void main(String[] args) {
+        System.out.println("Area      = " + (4.5 * 7.9));
+        System.out.println("Perimeter = " + (2 * (4.5 + 7.9)));
+    }
+}
