cf7b23baad6fa1ba8f7d95e97b8de6deeaa8c405
Comen35 <bahadir33@msn.com>
2022-05-31 18:43:32 +0300

    exercise explanation added.

--- Chapter1/src/chapter001Exercises/Exercise1_7.java
+++ Chapter1/src/chapter001Exercises/Exercise1_7.java
@@ -1,5 +1,5 @@
 package chapter001Exercises;
-
+// Approximate Pi;
 public class Exercise1_7 {
     public static void main(String[] args) {
         System.out.println("4 x ( 1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 = "
