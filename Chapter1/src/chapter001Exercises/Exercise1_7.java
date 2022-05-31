26b8438f11b57c48d496d888de98d0fba188c351
Comen35 <bahadir33@msn.com>
2022-05-31 18:41:45 +0300

    Chapter1 exercise 7 added

---
+++ Chapter1/src/chapter001Exercises/Exercise1_7.java
@@ -0,0 +1,13 @@
+package chapter001Exercises;
+
+public class Exercise1_7 {
+    public static void main(String[] args) {
+        System.out.println("4 x ( 1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 = "
+                + (4 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11))));
+        
+        System.out.println("4 x ( 1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + 1/13 = "
+                + (4 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11) + (1.0/13))));
+
+
+    }
+}
