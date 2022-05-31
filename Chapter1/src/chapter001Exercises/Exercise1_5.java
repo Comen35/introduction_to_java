0f8f6b0aa7d5e05d16e2cacf017cb53382bd7ae4
Comen35 <bahadir33@msn.com>
2022-05-31 18:32:48 +0300

    Chapter 1 exercise 5 added

---
+++ Chapter1/src/chapter001Exercises/Exercise1_5.java
@@ -0,0 +1,10 @@
+package chapter001Exercises;
+
+public class Exercise1_5 {
+    public static void main(String[] args) {
+        System.out.println("9.5 X 4.5 - 2.5 x 3");
+        System.out.println("---------------------" + " = " +(float)((9.5 * 4.5) - (2.5 * 4) / (45.5 - 3.5)));
+        System.out.println("    45.5 - 3.5     ");
+    }
+}
+
