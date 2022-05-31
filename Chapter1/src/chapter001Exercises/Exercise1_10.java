676c6fbc999843ee647c7864faee35d731dfb454
Comen35 <bahadir33@msn.com>
2022-05-31 19:16:55 +0300

    Chapter1 exercise 10 added

---
+++ Chapter1/src/chapter001Exercises/Exercise1_10.java
@@ -0,0 +1,9 @@
+package chapter001Exercises;
+
+public class Exercise1_10 {
+    public static void main(String[] args) {
+        double average_speed_in_miles_per_hour = ((14 / 1.6) * 60 * 60) / ((45 * 60) + 30);
+        System.out.println("The average speed in miles per hour : " + average_speed_in_miles_per_hour);
+        
+    }
+}
