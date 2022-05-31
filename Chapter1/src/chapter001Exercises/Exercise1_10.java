055280e8d6171f734ca8b1cbb0cb5cee93747afa
Comen35 <bahadir33@msn.com>
2022-05-31 19:19:35 +0300

    Chapter 1 exercise 10 added

--- Chapter1/src/chapter001Exercises/Exercise1_10.java
+++ Chapter1/src/chapter001Exercises/Exercise1_10.java
@@ -1,5 +1,5 @@
 package chapter001Exercises;
-
+/*  Assume that a runner runs 14 kilometers in 45 minutes and 30 seconds. Write a program that displays the average speed in miles per hour. */
 public class Exercise1_10 {
     public static void main(String[] args) {
         double average_speed_in_miles_per_hour = ((14 / 1.6) * 60 * 60) / ((45 * 60) + 30);
